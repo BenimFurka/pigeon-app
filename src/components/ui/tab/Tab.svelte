f5ff21af81b10db6205550be43e6257f9398df36
furka <furkanchik00@mail.ru>
2025-12-19 09:43:11 +0400

    tab

--- src/components/ui/tab/Tab.svelte
+++ src/components/ui/tab/Tab.svelte
@@ -1,34 +1,40 @@
 <script lang="ts">
-	export let text: string = '';
-	export let id: string = '';
-	export let active: boolean = false;
-	export let onClick: (e: MouseEvent) => void = (e) => {};
+    export let text: string = '';
+    export let id: string = '';
+    export let active: boolean = false;
+    export let onClick: (e: MouseEvent) => void = () => {};
 </script>
 
-<button id={id} class={`tab ${active ? 'active' : ''}`} on:click={onClick}>
-  	{text}
+<button 
+    id={id} 
+    class="tab"
+    class:active
+    on:click={onClick}
+>
+    {text}
 </button>
 
 <style>
-    /* TODO: animation */
     .tab {
         flex: 1;
         padding: 12px;
         text-align: center;
         cursor: pointer;
         color: var(--color-accent);
-        transition: var(--transition);
         background: none;
         border: none;
         outline: none;
         font: inherit;
         border-bottom: 2px solid var(--surface-glass);
+        transition: var(--transition), color ease 0.3s;
+        position: relative;
+        z-index: 1;
     }
-        
+
     .tab.active {
-        color: var(--color-accent);
-        background: var(--color-accent-soft);
-        border-bottom: 2px solid var(--color-accent);
+        color: var(--color-text);
+        background: none;
+        border-bottom: 2px solid transparent;
     }
 
     .tab:hover:not(.active) {
